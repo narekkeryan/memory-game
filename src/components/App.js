f486b2b156a9d900ea0a6dd4590e8501e73c9964
Narek Keryan <narek.keryan@gmail.com>
2018-08-04 00:17:15 +0400

    webpack config

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Hello from App Component</h1>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.js?/,
+      include: path.resolve(__dirname, './src'),
+      loader: 'babel-loader',
+      query: {
+        presets: ['react']
+      }
+    }]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
